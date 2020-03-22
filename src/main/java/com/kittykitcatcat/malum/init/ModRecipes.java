e0c54be58ded668f8fd2720062faf55fc5e05187
KittyCatKittyCat <elektroplaygames@gmail.com>
2020-03-22 01:11:16 +0100

    Recipe system

--- src/main/java/com/kittykitcatcat/malum/init/ModRecipes.java
+++ src/main/java/com/kittykitcatcat/malum/init/ModRecipes.java
@@ -2,7 +2,9 @@ package com.kittykitcatcat.malum.init;
 
 
 import com.kittykitcatcat.malum.recipes.BlockTransmutationRecipe;
+import com.kittykitcatcat.malum.recipes.SpiritFurnaceRecipe;
 import net.minecraft.block.BlockState;
+import net.minecraft.item.Item;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -19,6 +21,7 @@ public class ModRecipes
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void registerRecipes(FMLCommonSetupEvent event)
     {
+        SpiritFurnaceRecipe.initRecipes();
         BlockTransmutationRecipe.initRecipes();
     }
 
@@ -40,6 +43,32 @@ public class ModRecipes
             for (BlockTransmutationRecipe recipe : blockTransmutationRecipes)
             {
                 if (recipe.getBlock() == sourceBlock.getBlock())
+                {
+                    return recipe;
+                }
+            }
+        }
+        return null;
+    }
+
+
+    public static List<SpiritFurnaceRecipe> spiritFurnaceRecipes = new ArrayList<>();
+
+    public static void addSpiritFurnaceRecipe(SpiritFurnaceRecipe recipe)
+    {
+        if (!spiritFurnaceRecipes.contains(recipe))
+        {
+            spiritFurnaceRecipes.add(recipe);
+        }
+    }
+
+    public static SpiritFurnaceRecipe getSpiritFurnaceRecipe(Item item)
+    {
+        if (item != null)
+        {
+            for (SpiritFurnaceRecipe recipe : spiritFurnaceRecipes)
+            {
+                if (recipe.getInputItem().equals(item))
                 {
                     return recipe;
                 }
---
+++ src/main/java/com/kittykitcatcat/malum/recipes/SpiritFurnaceRecipe.java
@@ -0,0 +1,44 @@
+package com.kittykitcatcat.malum.recipes;
+
+import com.kittykitcatcat.malum.init.ModItems;
+import com.kittykitcatcat.malum.init.ModRecipes;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+
+public class SpiritFurnaceRecipe
+{
+
+    private Item inputItem;
+    private Item outputItem;
+    private int outputCount;
+    private int burnTime;
+    public SpiritFurnaceRecipe(Item inputItem, Item outputItem, int outputCount, int burnTime)
+    {
+        this.inputItem = inputItem;
+        this.outputItem = outputItem;
+        this.outputCount = outputCount;
+        this.burnTime = burnTime;
+    }
+
+    public Item getInputItem()
+    {
+        return inputItem;
+    }
+
+    public Item getOutputItem()
+    {
+        return outputItem;
+    }
+    public int getOutputCount()
+    {
+        return outputCount;
+    }
+    public int getBurnTime()
+    {
+        return burnTime;
+    }
+    public static void initRecipes()
+    {
+        ModRecipes.addSpiritFurnaceRecipe(new SpiritFurnaceRecipe(Items.DIAMOND, ModItems.vacant_gemstone, 1, 1600));
+    }
+}
