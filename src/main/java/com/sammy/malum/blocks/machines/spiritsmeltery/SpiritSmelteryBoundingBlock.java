781171524fea221ca6005a8aef5965e746da351b
Sammy <malummoddevelopment@gmail.com>
2020-10-30 05:48:56 +0100

    ? ?? ? ? ? ?

--- src/main/java/com/sammy/malum/blocks/machines/spiritsmeltery/SpiritSmelteryBoundingBlock.java
+++ src/main/java/com/sammy/malum/blocks/machines/spiritsmeltery/SpiritSmelteryBoundingBlock.java
@@ -3,10 +3,14 @@ package com.sammy.malum.blocks.machines.spiritsmeltery;
 import com.sammy.malum.blocks.utility.multiblock.BoundingBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.state.IntegerProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 
@@ -14,9 +18,9 @@ import static net.minecraft.state.properties.BlockStateProperties.HORIZONTAL_FAC
 
 public class SpiritSmelteryBoundingBlock extends BoundingBlock
 {
-    public static final IntegerProperty x = IntegerProperty.create("x", 0, 2);
+    public static final IntegerProperty x = IntegerProperty.create("x", -1, 1);
     public static final IntegerProperty y = IntegerProperty.create("y", 0, 2);
-    public static final IntegerProperty z = IntegerProperty.create("z", 0, 2);
+    public static final IntegerProperty z = IntegerProperty.create("z", -1, 1);
     
     public SpiritSmelteryBoundingBlock(Properties properties)
     {
@@ -28,6 +32,12 @@ public class SpiritSmelteryBoundingBlock extends BoundingBlock
         blockStateBuilder.add(x);
         blockStateBuilder.add(y);
         blockStateBuilder.add(z);
+    }
+    
+    @Override
+    public BlockState state(BlockPos placePos, World world, PlayerEntity player, ItemStack stack, BlockState state, BlockPos pos)
+    {
+        return super.state(placePos, world, player, stack, state, pos).with(x,pos.getX()).with(y,pos.getY()).with(z,pos.getZ());
     }
     
     @Nullable
