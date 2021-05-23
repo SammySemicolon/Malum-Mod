55ee3566696d5d989d8e2f5366f3288e248fa1ca
Sammy <malummoddevelopment@gmail.com>
2021-05-23 15:59:28 +0200

    Spirit altar now properly drops it's contents

--- src/main/java/com/sammy/malum/common/blocks/spiritaltar/SpiritAltarBlock.java
+++ src/main/java/com/sammy/malum/common/blocks/spiritaltar/SpiritAltarBlock.java
@@ -1,11 +1,14 @@
 package com.sammy.malum.common.blocks.spiritaltar;
 
 import com.sammy.malum.MalumHelper;
+import com.sammy.malum.common.blocks.itemstand.ItemStandBlock;
+import com.sammy.malum.common.blocks.itemstand.ItemStandTileEntity;
 import com.sammy.malum.common.items.SpiritItem;
 import com.sammy.malum.core.init.MalumSounds;
 import com.sammy.malum.core.init.items.MalumItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
@@ -16,6 +19,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 
 public class SpiritAltarBlock extends Block
 {
@@ -23,7 +27,34 @@ public class SpiritAltarBlock extends Block
     {
         super(properties);
     }
-    
+
+    @Override
+    public void onBlockHarvested(World worldIn, BlockPos pos, BlockState state, PlayerEntity player)
+    {
+        if (worldIn instanceof ServerWorld)
+        {
+            spawnAdditionalDrops(state, (ServerWorld) worldIn, pos, player.getActiveItemStack());
+        }
+        super.onBlockHarvested(worldIn, pos, state, player);
+    }
+    @Override
+    public void spawnAdditionalDrops(BlockState state, ServerWorld worldIn, BlockPos pos, ItemStack stack)
+    {
+        if (worldIn.getTileEntity(pos) instanceof SpiritAltarTileEntity)
+        {
+            SpiritAltarTileEntity tileEntity = (SpiritAltarTileEntity) worldIn.getTileEntity(pos);
+            worldIn.addEntity(new ItemEntity(worldIn,pos.getX()+0.5f,pos.getY()+0.5f,pos.getZ()+0.5f,tileEntity.inventory.getStackInSlot(0)));
+            for (ItemStack itemStack : tileEntity.spiritInventory.stacks())
+            {
+                worldIn.addEntity(new ItemEntity(worldIn,pos.getX()+0.5f,pos.getY()+0.5f,pos.getZ()+0.5f,itemStack));
+            }
+            for (ItemStack itemStack : tileEntity.extrasInventory.stacks())
+            {
+                worldIn.addEntity(new ItemEntity(worldIn,pos.getX()+0.5f,pos.getY()+0.5f,pos.getZ()+0.5f,itemStack));
+            }
+        }
+        super.spawnAdditionalDrops(state, worldIn, pos, stack);
+    }
     @Override
     public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit)
     {
