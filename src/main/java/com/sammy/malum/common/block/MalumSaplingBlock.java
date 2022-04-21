b8af6d333adf8f9e8d77bc916b2e32352dad7999
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2022-04-21 22:35:26 +0200

    Bug Fixes

--- src/main/java/com/sammy/malum/common/block/MalumSaplingBlock.java
+++ src/main/java/com/sammy/malum/common/block/MalumSaplingBlock.java
@@ -1,5 +1,6 @@
 package com.sammy.malum.common.block;
 
+import com.sammy.malum.common.worldgen.RunewoodTreeFeature;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.block.SaplingBlock;
@@ -7,35 +8,29 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
+import net.minecraftforge.registries.RegistryObject;
 
 import java.util.Optional;
 import java.util.Random;
 import java.util.function.Supplier;
 
-public class MalumSaplingBlock extends SaplingBlock
-{
-    public final Supplier<Feature<NoneFeatureConfiguration>> tree;
-    public MalumSaplingBlock(Properties properties, Supplier<Feature<NoneFeatureConfiguration>> tree)
-    {
+public class MalumSaplingBlock extends SaplingBlock {
+    public final Supplier<? extends Feature<NoneFeatureConfiguration>> tree;
+
+    public MalumSaplingBlock(Properties properties, RegistryObject<? extends Feature<NoneFeatureConfiguration>> tree) {
         super(null, properties);
         this.tree = tree;
     }
 
     @Override
-    public void advanceTree(ServerLevel level, BlockPos pos, BlockState state, Random rand)
-    {
-        if (state.getValue(STAGE) == 0)
-        {
+    public void advanceTree(ServerLevel level, BlockPos pos, BlockState state, Random rand) {
+        if (state.getValue(STAGE) == 0) {
             level.setBlock(pos, state.cycle(STAGE), 4);
-        }
-        else
-        {
-            if (!net.minecraftforge.event.ForgeEventFactory.saplingGrowTree(level, rand, pos))
-            {
+        } else {
+            if (!net.minecraftforge.event.ForgeEventFactory.saplingGrowTree(level, rand, pos)) {
                 return;
             }
-            tree.get().place(new FeaturePlaceContext(Optional.empty(), level, level.getChunkSource().getGenerator(), rand,pos, NoneFeatureConfiguration.INSTANCE));
+            tree.get().place(NoneFeatureConfiguration.INSTANCE, level, level.getChunkSource().getGenerator(), rand, pos);
         }
     }
-    
 }
--- src/main/java/com/sammy/malum/compability/jei/JeiCompat.java
+++ src/main/java/com/sammy/malum/compability/jei/JeiCompat.java
@@ -2,14 +2,7 @@ package com.sammy.malum.compability.jei;
 
 import mezz.jei.api.runtime.IRecipesGui;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.projectile.Arrow;
-import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.ModList;
-import se.mickelus.tetra.aspect.ItemAspect;
-import se.mickelus.tetra.effect.ItemEffect;
-import se.mickelus.tetra.items.modular.ModularItem;
 
 public class JeiCompat {
     public static boolean LOADED;
