d62a7eaf0a3845717fc837a027dec5d2a892ef4e
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2022-07-04 02:37:56 +0200

    Mixin fix

--- src/main/java/com/sammy/malum/mixin/ForgeIngameGuiMixin.java
+++ src/main/java/com/sammy/malum/mixin/ForgeIngameGuiMixin.java
@@ -7,15 +7,16 @@ import net.minecraft.client.gui.Gui;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraftforge.client.gui.ForgeIngameGui;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Gui.class)
-public class GuiMixin {
+@Mixin(ForgeIngameGui.class)
+public class ForgeIngameGuiMixin {
 
-    @ModifyArg(method = "renderPlayerHealth", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;hasEffect(Lnet/minecraft/world/effect/MobEffect;)Z", ordinal = 1))
+    @ModifyArg(method = "renderFood", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;hasEffect(Lnet/minecraft/world/effect/MobEffect;)Z"))
     private MobEffect malumGluttonyIsVisuallyHungerMixin(MobEffect effect) {
         LocalPlayer player = Minecraft.getInstance().player;
         if (effect.equals(MobEffects.HUNGER) && player.hasEffect(MalumMobEffectRegistry.GLUTTONY.get())) {
