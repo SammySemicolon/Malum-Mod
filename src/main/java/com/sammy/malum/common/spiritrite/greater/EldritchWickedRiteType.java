a743c823cf0949293887a353e85df86ba3071e72
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2023-01-22 23:54:48 +0100

    Update EldritchWickedRiteType.java

--- src/main/java/com/sammy/malum/common/spiritrite/greater/EldritchWickedRiteType.java
+++ src/main/java/com/sammy/malum/common/spiritrite/greater/EldritchWickedRiteType.java
@@ -12,6 +12,7 @@ import net.minecraftforge.network.PacketDistributor;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.sammy.malum.registry.common.SpiritTypeRegistry.*;
 import static com.sammy.malum.registry.common.PacketRegistry.MALUM_CHANNEL;
@@ -41,12 +42,13 @@ public class EldritchWickedRiteType extends MalumRiteType {
         return new EntityAffectingRiteEffect() {
             @Override
             public void riteEffect(TotemBaseBlockEntity totemBase) {
-                List<Animal> entities = getNearbyEntities(totemBase, Animal.class, e -> !e.isInLove() && e.getAge() > 0 && !e.isInvulnerableTo(DamageSourceRegistry.VOODOO)).collect(Collectors.toList());
-                if (entities.size() < 30) {
+                final List<Animal> nearbyEntities = getNearbyEntities(totemBase, Animal.class, e -> e.getAge() > 0 && !e.isInvulnerableTo(DamageSourceRegistry.VOODOO)).collect(Collectors.toList());
+                if (nearbyEntities.size() < 30) {
                     return;
                 }
-                int maxKills = entities.size() - 30;
-                for (Animal entity : entities) {
+                int maxKills = nearbyEntities.size() - 30;
+                nearbyEntities.removeIf(Animal::isInLove);
+                for (Animal entity : nearbyEntities) {
                     entity.hurt(DamageSourceRegistry.VOODOO, entity.getMaxHealth());
                     MALUM_CHANNEL.send(PacketDistributor.TRACKING_ENTITY.with(() -> entity), new MajorEntityEffectParticlePacket(WICKED_SPIRIT.getColor(), entity.getX(), entity.getY() + entity.getBbHeight() / 2f, entity.getZ()));
                     if (maxKills-- <= 0) {
