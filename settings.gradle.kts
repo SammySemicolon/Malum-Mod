31a27968f214c5428990b7090026f919424d07c2
EpicSquid <epicsquid315@gmail.com>
2023-07-16 07:44:44 +1000

    1.20 gradle

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+	repositories {
+		gradlePluginPortal()
+		mavenCentral()
+		maven("https://maven.minecraftforge.net/")
+		maven("https://maven.parchmentmc.org")
+		maven("https://repo.spongepowered.org/repository/maven-public/")
+	}
+	resolutionStrategy {
+		eachPlugin {
+			if (requested.id.id == "org.spongepowered.mixin") {
+				useModule("org.spongepowered:mixingradle:0.7-SNAPSHOT")
+			}
+		}
+	}
+}
+
+rootProject.name = "Lodestone"
+
+plugins {
+	id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
