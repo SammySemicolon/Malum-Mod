abc84ca9ea12fb74c0e861d69803c05cf72bf732
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2024-04-24 18:24:05 +0200

    twooo days till boyfrreidndd,d

--- src/main/java/com/sammy/malum/data/recipe/infusion/MaterialSpiritInfusionRecipes.java
+++ src/main/java/com/sammy/malum/data/recipe/infusion/MaterialSpiritInfusionRecipes.java
@@ -124,5 +124,27 @@ public class MaterialSpiritInfusionRecipes {
                 .addExtraItem(ItemRegistry.VOID_SALTS.get(), 8)
                 .addSpirit(ELDRITCH_SPIRIT, 4)
                 .build(consumer);
+
+        new SpiritInfusionRecipeBuilder(ItemRegistry.RUNEWOOD_LOG.get(), 2, ItemRegistry.RUNEWOOD_TABLET.get(), 2)
+                .addExtraItem(ItemRegistry.PROCESSED_SOULSTONE.get(), 8)
+                .addExtraItem(ItemRegistry.RUNEWOOD_PLANKS.get(), 4)
+                .addExtraItem(ItemRegistry.HEX_ASH.get(), 2)
+                .addSpirit(AERIAL_SPIRIT, 8)
+                .addSpirit(AQUEOUS_SPIRIT, 8)
+                .addSpirit(EARTHEN_SPIRIT, 8)
+                .addSpirit(INFERNAL_SPIRIT, 8)
+                .addSpirit(ELDRITCH_SPIRIT, 2)
+                .build(consumer);
+
+        new SpiritInfusionRecipeBuilder(ItemRegistry.SOULWOOD_LOG.get(), 2, ItemRegistry.SOULWOOD_TABLET.get(), 2)
+                .addExtraItem(ItemRegistry.PROCESSED_SOULSTONE.get(), 8)
+                .addExtraItem(ItemRegistry.SOULWOOD_PLANKS.get(), 4)
+                .addExtraItem(ItemRegistry.HEX_ASH.get(), 2)
+                .addSpirit(AERIAL_SPIRIT, 8)
+                .addSpirit(AQUEOUS_SPIRIT, 8)
+                .addSpirit(EARTHEN_SPIRIT, 8)
+                .addSpirit(INFERNAL_SPIRIT, 8)
+                .addSpirit(ELDRITCH_SPIRIT, 2)
+                .build(consumer);
     }
 }
