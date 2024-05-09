9b78eea8531ad0a27c3c45d400c796a78728e811
SammySemicolon <malummoddevelopment@gmail.com>
2024-05-09 23:35:16 +0200

    Update WoodTypeRegistry.java

--- src/main/java/com/sammy/malum/registry/common/block/WoodTypeRegistry.java
+++ src/main/java/com/sammy/malum/registry/common/block/WoodTypeRegistry.java
@@ -1,8 +1,12 @@
 package com.sammy.malum.registry.common.block;
 
 import com.sammy.malum.MalumMod;
+import net.minecraft.client.renderer.Sheets;
 import net.minecraft.world.level.block.state.properties.WoodType;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +21,17 @@ public class WoodTypeRegistry {
         public MalumWoodType(String nameIn) {
             super("malum:" + nameIn);
             WOOD_TYPES.add(this);
+        }
+    }
+
+    @Mod.EventBusSubscriber(modid= MalumMod.MALUM, bus= Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientOnly {
+        @SubscribeEvent
+        public static void addWoodTypes(FMLClientSetupEvent event) {
+            event.enqueueWork(() -> {
+                Sheets.addWoodType(RUNEWOOD);
+                Sheets.addWoodType(SOULWOOD);
+            });
         }
     }
 }
