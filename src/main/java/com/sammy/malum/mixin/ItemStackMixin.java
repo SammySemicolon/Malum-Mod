c442b44b933461269aa04102609d07a71b8ef591
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2024-05-26 04:20:23 +0200

    Update ItemStackMixin.java

--- src/main/java/com/sammy/malum/mixin/ItemStackMixin.java
+++ src/main/java/com/sammy/malum/mixin/ItemStackMixin.java
@@ -1,34 +1,22 @@
 package com.sammy.malum.mixin;
 
-import com.google.common.collect.LinkedHashMultimap;
-import com.google.common.collect.Multimap;
-import com.sammy.malum.common.item.curiosities.weapons.scythe.MalumScytheItem;
-import com.sammy.malum.registry.common.AttributeRegistry;
-import com.sammy.malum.registry.common.item.EnchantmentRegistry;
-import net.minecraft.world.entity.ai.attributes.Attribute;
-import net.minecraft.world.entity.ai.attributes.AttributeInstance;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
-import org.jetbrains.annotations.Nullable;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import team.lodestar.lodestone.registry.common.LodestoneAttributeRegistry;
+import com.google.common.collect.*;
+import com.sammy.malum.common.item.curiosities.weapons.scythe.*;
+import com.sammy.malum.registry.common.*;
+import net.minecraft.world.entity.ai.attributes.*;
+import net.minecraft.world.entity.player.*;
+import net.minecraft.world.item.*;
+import org.jetbrains.annotations.*;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.*;
 
-import java.util.Map;
+import java.util.*;
 
-import static net.minecraft.world.item.Item.BASE_ATTACK_DAMAGE_UUID;
+import static net.minecraft.world.item.Item.*;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
 
-    @Shadow
-    public abstract Item getItem();
 
     @ModifyVariable(method = "getTooltipLines", at = @At("STORE"))
     private Multimap<Attribute, AttributeModifier> malum$getTooltip(Multimap<Attribute, AttributeModifier> map, @Nullable Player player, TooltipFlag flag) {
@@ -40,7 +28,8 @@ public abstract class ItemStackMixin {
                 double amount = modifier.getAmount();
 
                 if (modifier.getId() != null) {
-                    if (modifier.getId().equals(BASE_ATTACK_DAMAGE_UUID) && getItem() instanceof MalumScytheItem) {
+                    final Item item = ((ItemStack) ((Object) this)).getItem();
+                    if (modifier.getId().equals(BASE_ATTACK_DAMAGE_UUID) && item instanceof MalumScytheItem) {
                         AttributeInstance instance = player.getAttribute(AttributeRegistry.SCYTHE_PROFICIENCY.get());
                         if (instance != null && instance.getValue() > 0) {
                             amount += instance.getValue() * 0.5f;
