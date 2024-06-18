40b4fa1b4296ff5e2ef7780f99aaef3e823f8248
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2024-06-18 17:59:54 +0200

    Bump lodestone version

--- src/main/java/com/sammy/malum/registry/client/ShaderRegistry.java
+++ src/main/java/com/sammy/malum/registry/client/ShaderRegistry.java
@@ -1,15 +1,13 @@
 package com.sammy.malum.registry.client;
 
-import com.mojang.blaze3d.vertex.DefaultVertexFormat;
-import com.sammy.malum.MalumMod;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RegisterShadersEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+import com.mojang.blaze3d.vertex.*;
+import com.sammy.malum.*;
+import net.minecraftforge.api.distmarker.*;
+import net.minecraftforge.client.event.*;
+import net.minecraftforge.eventbus.api.*;
+import net.minecraftforge.fml.common.*;
 import team.lodestar.lodestone.registry.client.*;
-import team.lodestar.lodestone.systems.rendering.shader.ShaderHolder;
-
-import java.io.IOException;
+import team.lodestar.lodestone.systems.rendering.shader.*;
 
 @Mod.EventBusSubscriber(value = Dist.CLIENT, modid = MalumMod.MALUM, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ShaderRegistry {
@@ -18,8 +16,8 @@ public class ShaderRegistry {
     public static ShaderHolder DISTORTION = new ShaderHolder(MalumMod.malumPath("distortion"), DefaultVertexFormat.POSITION_COLOR_TEX_LIGHTMAP, "Speed", "Distortion", "Width", "Height", "UVEncasement");
 
     @SubscribeEvent
-    public static void shaderRegistry(RegisterShadersEvent event) throws IOException {
-        LodestoneShaderRegistry.registerShader(event, TOUCH_OF_DARKNESS.createInstance(event.getResourceProvider()));
-        LodestoneShaderRegistry.registerShader(event, DISTORTION.createInstance(event.getResourceProvider()));
+    public static void shaderRegistry(RegisterShadersEvent event) {
+        LodestoneShaderRegistry.registerShader(event, TOUCH_OF_DARKNESS);
+        LodestoneShaderRegistry.registerShader(event, DISTORTION);
     }
 }
