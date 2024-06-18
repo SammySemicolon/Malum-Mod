d21ef052d3e5d36f3e734e80ea68d4e550e19285
yrsegal@gmail.com <yrsegal@gmail.com>
2024-06-18 14:20:49 -0400

    fix double cycle on end of infusion

--- src/main/java/com/sammy/malum/common/block/curiosities/spirit_altar/SpiritAltarBlockEntity.java
+++ src/main/java/com/sammy/malum/common/block/curiosities/spirit_altar/SpiritAltarBlockEntity.java
@@ -309,7 +309,7 @@ public class SpiritAltarBlockEntity extends LodestoneBlockEntity {
                 }
             }
 
-            return false;
+            return AltarCraftingHelper.extractIngredient(extrasInventory, nextIngredient.ingredient, nextIngredient.count, true).isEmpty();
         }
 
         return true;
