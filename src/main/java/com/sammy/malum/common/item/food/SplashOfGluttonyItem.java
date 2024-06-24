0fc083e01503ddb04933f4e17da2e110d5c758fb
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2024-06-24 13:16:08 +0200

    Sounm,bd

--- src/main/java/com/sammy/malum/common/item/food/SplashOfGluttonyItem.java
+++ src/main/java/com/sammy/malum/common/item/food/SplashOfGluttonyItem.java
@@ -1,14 +1,15 @@
 package com.sammy.malum.common.item.food;
 
 import com.sammy.malum.common.entity.thrown.*;
+import net.minecraft.sounds.*;
 import net.minecraft.stats.*;
 import net.minecraft.world.*;
 import net.minecraft.world.entity.player.*;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.*;
 
-public class ThrownConcentratedGluttonyItem extends Item {
-    public ThrownConcentratedGluttonyItem(Properties pProperties) {
+public class SplashOfGluttonyItem extends Item {
+    public SplashOfGluttonyItem(Properties pProperties) {
         super(pProperties);
     }
 
@@ -20,6 +21,7 @@ public class ThrownConcentratedGluttonyItem extends Item {
             thrownGluttony.setItem(itemstack);
             thrownGluttony.shootFromRotation(pPlayer, pPlayer.getXRot(), pPlayer.getYRot(), -20.0F, 0.5F, 1.0F);
             pLevel.addFreshEntity(thrownGluttony);
+            pPlayer.playSound(SoundEvents.SPLASH_POTION_THROW, 0.5f, 0.4F / (pLevel.getRandom().nextFloat() * 0.4F + 0.8F));
         }
 
         pPlayer.awardStat(Stats.ITEM_USED.get(this));
--- src/main/java/com/sammy/malum/registry/common/item/ItemRegistry.java
+++ src/main/java/com/sammy/malum/registry/common/item/ItemRegistry.java
@@ -632,7 +632,7 @@ public class ItemRegistry {
     public static final RegistryObject<Item> VIVID_NITRATE = register("vivid_nitrate", HIDDEN_PROPERTIES(), VividNitrateItem::new);
 
     public static final RegistryObject<Item> CONCENTRATED_GLUTTONY = register("concentrated_gluttony", DEFAULT_PROPERTIES().food(FoodPropertyRegistry.CONCENTRATED_GLUTTONY), ConcentratedGluttonyItem::new);
-    public static final RegistryObject<Item> SPLASH_OF_GLUTTONY = register("splash_of_gluttony", DEFAULT_PROPERTIES(), ThrownConcentratedGluttonyItem::new);
+    public static final RegistryObject<Item> SPLASH_OF_GLUTTONY = register("splash_of_gluttony", DEFAULT_PROPERTIES(), SplashOfGluttonyItem::new);
 
 
     public static final RegistryObject<Item> CRUDE_SCYTHE = register("crude_scythe", GEAR_PROPERTIES(), (p) -> new MalumScytheItem(Tiers.IRON, 0, 0.1f, p.durability(500)));
