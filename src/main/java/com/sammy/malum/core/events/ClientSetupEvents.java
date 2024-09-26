448c53a100eb0c5392062f66d2a234bda60a9925
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2024-09-26 17:59:04 +0200

    Move this goober

--- src/main/java/com/sammy/malum/core/events/ClientSetupEvents.java
+++ src/main/java/com/sammy/malum/core/events/ClientSetupEvents.java
@@ -1,15 +1,23 @@
 package com.sammy.malum.core.events;
 
+import com.sammy.malum.common.item.spirit.*;
 import com.sammy.malum.core.handlers.*;
 import com.sammy.malum.registry.client.*;
+import com.sammy.malum.registry.common.item.*;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterParticleProvidersEvent;
+import net.neoforged.neoforge.client.extensions.common.*;
 
 @EventBusSubscriber(value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
 public class ClientSetupEvents {
 
+
+    @SubscribeEvent
+    public static void registerClientExtensions(RegisterClientExtensionsEvent event) {
+        event.registerItem(SpiritJarItem.CUSTOM_RENDERER, ItemRegistry.SPIRIT_JAR.get());
+    }
 
     @SubscribeEvent
     public static void registerOverlays(RegisterGuiOverlaysEvent event) {
