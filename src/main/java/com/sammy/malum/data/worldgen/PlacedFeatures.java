7f133c6db0ed2ed5206ef4c4ed0b583738932ce9
TJT01 <87955906+TJT01@users.noreply.github.com>
2025-02-26 18:55:06 -0500

    Adjusted ore placement counts Brilliant: 3 -> 12 Natural Quartz: 2 -> 16 Soulstone: 3 -> 20

--- src/main/java/com/sammy/malum/data/worldgen/PlacedFeatures.java
+++ src/main/java/com/sammy/malum/data/worldgen/PlacedFeatures.java
@@ -17,9 +17,9 @@ import java.util.*;
 public class PlacedFeatures {
     public static void bootstrap(BootstrapContext<PlacedFeature> context) {
         HolderGetter<ConfiguredFeature<?, ?>> features = context.lookup(Registries.CONFIGURED_FEATURE);
-        context.register(PlacedFeatureRegistry.ORE_SOULSTONE, addOreFeature(features.getOrThrow(ConfiguredFeatureRegistry.CONFIGURED_SOULSTONE_ORE), -64, 100, 3));
-        context.register(PlacedFeatureRegistry.ORE_BRILLIANT, addOreFeature(features.getOrThrow(ConfiguredFeatureRegistry.CONFIGURED_BRILLIANT_ORE), -64, 40, 3));
-        context.register(PlacedFeatureRegistry.ORE_NATURAL_QUARTZ, addOreFeature(features.getOrThrow(ConfiguredFeatureRegistry.CONFIGURED_NATURAL_QUARTZ_ORE), -64, 10, 2));
+        context.register(PlacedFeatureRegistry.ORE_SOULSTONE, addOreFeature(features.getOrThrow(ConfiguredFeatureRegistry.CONFIGURED_SOULSTONE_ORE), -64, 100, 20));
+        context.register(PlacedFeatureRegistry.ORE_BRILLIANT, addOreFeature(features.getOrThrow(ConfiguredFeatureRegistry.CONFIGURED_BRILLIANT_ORE), -64, 40, 12));
+        context.register(PlacedFeatureRegistry.ORE_NATURAL_QUARTZ, addOreFeature(features.getOrThrow(ConfiguredFeatureRegistry.CONFIGURED_NATURAL_QUARTZ_ORE), -64, 10, 16));
         context.register(PlacedFeatureRegistry.ORE_BLAZING_QUARTZ, addOreFeature(features.getOrThrow(ConfiguredFeatureRegistry.CONFIGURED_BLAZING_QUARTZ_ORE), -16, 112, 16));
         context.register(PlacedFeatureRegistry.ORE_CTHONIC_GOLD, addOreFeature(features.getOrThrow(ConfiguredFeatureRegistry.CONFIGURED_CTHONIC_GOLD_ORE_FEATURE), -48, -0, 1, RarityFilter.onAverageOnceEvery(2)));
 
